2759c8195b760cdfe53cf592d3908c20a753a771
fabienbrun <fabienbrun@hotmail.fr>
2016-01-29 11:55:50 +0100

    add & clean files -Serveur Good

--- serveur.js
+++ serveur.js
@@ -1,22 +1,44 @@
-/**
- * Created by fabienbrun on 29/01/16.
- */
-// module HTTP intégré à NodeJS : appeler la méthode require qui prend en paratres (le nom du module ou l'emplacement )
-//    De manière synchrone on vient charger le module HTTP
 var http = require("http");
-// On définie une variable du PORT sur lequel le serveur va ecouter
+var fs = require("fs");
+var mime = require("mime"); // installer le module mime depuis la console à la racine du projet , avec "npm install mime"
+var express = require("express"); // installer le module connect depuis la console à la racine du projet , avec "npm install connect"
+var serviceMails = require(__dirname + "/get-mails.js");
+// midldlewares
+var logger = require('morgan');
+var bodyParser = require("body-parser");
+
+var serveStatic = require("serve-static"); // Middlewere sous forme de module static  connect serveStatic  : "npm install serve-static"
+
 var PORT = 8080;
 
-// Dans le module HTTP , on a une méthode "createServer" qui prend en paramètre une fonction (le fameu Call back)
-// Cette fonction prend en parametres req et res , la requette envoyé par le client et la reponse renvoyée
-// Une fois que le serveur est lancé il faut déclencher l'écoute en appelant la méthode listen
-// on passe en paramètre le port d'écoute
+serviceMails.genererMails();
+var app = express();
 
-http.createServer(function(req, res) {
-    // Reponses au client / la méthode end indique l'on a fini d'écrire la réponse
-    // On peut avant d'écrire la réponse , venir écrire les headers ainsi que le code de réponse HTTP 200=tout va bien
-    res.writeHead(200,{ "Content-Type" : "text/plain" });
-    res.end("Hello Worlds");
-}).listen(PORT);
 
-console.log("Serveur démaré sur le port :" + PORT);
+app.use(logger(":method :url"));
+app.use(serveStatic(__dirname ));
+
+// API
+var api = express();
+
+// Récupérer la liste des dossiers
+// GET /api/dossiers
+api.get("/dossiers", serviceMails.getDossiers);
+
+// Récupérer un dossier
+// GET /api/dossiers/idDossier
+
+api.get("/dossiers/:idDossier", serviceMails.getDossier);
+
+// Récupérer un mail
+// GET /api/dossier/idDossier/idMail
+api.get("/dossiers/:idDossier/:idMail", serviceMails.getMail);
+
+app.use(bodyParser.json());
+
+// Envoyer un mail
+// POST /api/envoi
+api.post("/envoi", serviceMails.envoiMail);
+app.use("/api", api);
+http.createServer(app).listen(PORT);
+console.log("Serveur démarré sur le port " + PORT);
